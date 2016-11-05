97c76e5b3ce3dbc6746ee3a86762933731ad2789
eacoeytaux <eacoeytaux@gmail.com>
2016-11-05 16:34:02 -0400

    updated gitignore and added ability to put islands inside island holes

--- src/Island.cpp
+++ src/Island.cpp
@@ -8,6 +8,8 @@
 
 #include "Island.hpp"
 
+#include "Utility.hpp"
+
 using namespace mapmqp;
 using namespace std;
 
@@ -15,23 +17,34 @@ Island::Island(const Polygon & mainPolygon, vector<shared_ptr<const MeshFace>> p
 mainPolygon_(mainPolygon),
 p_mainPolygonMeshFaces_(p_mainPolygonMeshFaces) { }
 
-const Polygon & Island::mainPolygon() {
+const Polygon & Island::mainPolygon() const {
     return mainPolygon_;
 }
 
-const vector<shared_ptr<const MeshFace>> & Island::p_mainPolygonMeshFaces() {
+const vector<shared_ptr<const MeshFace>> & Island::p_mainPolygonMeshFaces() const {
     return p_mainPolygonMeshFaces_;
 }
 
-const vector<Polygon> & Island::holes() {
+const vector<Polygon> & Island::holes() const {
     return holes_;
 }
 
-const vector<vector<shared_ptr<const MeshFace>>> & Island::p_holesMeshFaces() {
+const vector<vector<shared_ptr<const MeshFace>>> & Island::p_holesMeshFaces() const {
     return p_holesMeshFaces_;
 }
 
 void Island::addHole(Polygon hole, vector<shared_ptr<const MeshFace>> p_holeMeshFaces) {
     holes_.push_back(hole);
     p_holesMeshFaces_.push_back(p_holeMeshFaces);
+    holeIslands_.resize(holes_.size());
+}
+
+void Island::addIslandToHole(Island island, int holeIndex) {
+    if (holeIndex < 0) {
+        writeLog(WARNING, "attempting to add island to hole with index less than 0");
+    } else if (holeIndex >= holeIslands_.size()) {
+        writeLog(WARNING, "attempting to add island to hole index that does not exist");
+    } else {
+        holeIslands_[holeIndex].push_back(island);
+    }
 }
